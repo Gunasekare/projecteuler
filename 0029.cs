c7f70a683726d0d3aa73086afeb7f1dd5d8b8a14
Fabian De Alwis Gunasekare <fabian.alwis@gmail.com>
2020-05-20 19:18:41 +0530

    Problem 29

---
+++ 0029.cs
@@ -0,0 +1,32 @@
+﻿//Problem - Euler Problem #29 (Distinct powers)
+//URL     - https://projecteuler.net/problem=29
+//Author  - Gunasekare
+//Date    - 2020-05-20
+//Version - 1 (Naive)
+
+using System;
+using System.Collections.Generic;
+using System.Numerics;
+
+namespace _0029
+{
+    class Program
+    {
+        const int limit = 100;
+        static void Main(string[] args)
+        {
+            List <string> terms = new List<string>();
+
+            for (int a = 2; a <= limit; ++a)
+                for (int b = 2; b <= limit; ++b)
+                {
+                    BigInteger number = BigInteger.Pow(a, b);
+
+                    if (!terms.Contains(number.ToString()))
+                        terms.Add(number.ToString());
+                }
+
+            Console.Write(terms.Count);
+        }
+    }
+}
