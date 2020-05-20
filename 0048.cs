5428740162bf9ef30551b1af46e1a4b3f3f21844
Fabian De Alwis Gunasekare <fabian.alwis@gmail.com>
2020-05-20 18:59:50 +0530

    Problem 48

---
+++ 0048.cs
@@ -0,0 +1,27 @@
+﻿//Problem - Euler Problem #48 (Self powers)
+//URL     - https://projecteuler.net/problem=48
+//Author  - Gunasekare
+//Date    - 2020-05-20
+//Version - 1 (Naive)
+//Des     - I'm not too happy with this solution though.
+
+using System;
+using System.Numerics;
+
+namespace _0048
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BigInteger sum = 0;
+
+            for (int i = 1; i <= 1000; ++i)
+                sum += BigInteger.Pow(i, i);
+
+            string value = sum.ToString();
+            Console.Write(value.Substring(value.Length - 10));
+            
+        }
+    }
+}
