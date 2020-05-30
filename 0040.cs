a671cfe0ca315a38c3e45e51eb091e240059843b
Fabian De Alwis Gunasekare <fabian.alwis@gmail.com>
2020-05-30 11:30:05 +0530

    Problem 40

---
+++ 0040.cs
@@ -0,0 +1,26 @@
+﻿//Problem - Euler Problem #40 (Champernowne's constant)
+//URL     - https://projecteuler.net/problem=40
+//Author  - Gunasekare
+//Date    - 2020-05-30
+//Version - 1 (brute force - execution time 00:02:54.4728350, could be optimized further)
+
+using System;
+
+namespace _0040
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string value = "";
+
+            for (int i = 1; value.Length < 1000000; ++i)
+                value += i.ToString();
+
+            Console.Write(int.Parse(value[0].ToString()) * int.Parse(value[9].ToString()) 
+                                    * int.Parse(value[99].ToString()) * int.Parse(value[999].ToString())
+                                    * int.Parse(value[9999].ToString()) * int.Parse(value[99999].ToString())
+                                    * int.Parse(value[999999].ToString()));
+        }
+    }
+}
